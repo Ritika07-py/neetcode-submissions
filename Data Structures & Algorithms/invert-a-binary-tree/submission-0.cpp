d27113dfbe40611dc585b244f6d82a51e0fadd08
Ritika07-py <ritikadhankhark7@gmail.com>
2026-08-11 19:14:30 +0530

    Add: invert-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-0.cpp
@@ -0,0 +1,18 @@
+
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (!root) return nullptr;
+        queue<TreeNode*> queue;
+        queue.push(root);
+        while (!queue.empty()) {
+            TreeNode* node = queue.front();
+            queue.pop();
+            swap(node->left, node->right);
+            if (node->left) queue.push(node->left);
+            if (node->right) queue.push(node->right);
+        }
+        return root;
+    }
+};
